dd268ddc37f2f20ed276681972e1b835ec3282a9
Montoya1991 <jdmontoya0491@hotmail.com>
2023-04-19 02:59:34 -0500

    add

--- CrudBlazorServerApp1/Repositorio/IRepositorio.cs
+++ CrudBlazorServerApp1/Repositorio/IRepositorio.cs
@@ -4,10 +4,10 @@ namespace CrudBlazorServerApp1.Repositorio
     public interface IRepositorio
     {
         public Task<List<Book>> GetBooks();
-        public Task<Book> GetBookId(int BookId);
-        public Task<Book> CreateBook(Book CreateBook);
-        public Task<Book> UpdateBook(int BookId, Book UpdateBook);
-        public Task DeleteBook(int BookId);
+        public Task<Book> GetBookId(int libroId);
+        public Task<Book> crearLibro(Book createBook);
+        public Task<Book> ActualizarLibro(int libroId, Book actualizarLibro);
+        public Task DeleteBook(int libroId);
 
 
     }
--- CrudBlazorServerApp1/Repositorio/Repositorio.cs
+++ CrudBlazorServerApp1/Repositorio/Repositorio.cs
@@ -12,13 +12,29 @@ namespace CrudBlazorServerApp1.Repositorio
         {
             _dbContext = dBContext;
         }
-        public async Task<Book> CreateBook(Book CreateBook)
+/*En resumen, la línea de código "private readonly AplicationDBContext _dbContext;" es una variable de instancia de solo lectura que se utiliza para mantener una conexión segura y estable
+ con una base de datos en una aplicación C#. La sintaxis de asignación utilizada en el constructor permite que la instancia de "AplicationDBContext" se proporcione externamente, 
+lo que permite la inyección de dependencias y ayuda a lograr una mejor modularidad, escalabilidad y flexibilidad en la aplicación.*/
+        public async Task<Book> ActualizarLibro(int BookId, Book UpdateBook)
+        {
+            var LibroDesdeDB = await _dbContext.Books.FindAsync(BookId);
+            LibroDesdeDB.Title = UpdateBook.Title;
+            LibroDesdeDB.Description = UpdateBook.Description;
+            LibroDesdeDB.Autor = UpdateBook.Autor;
+            LibroDesdeDB.Page = UpdateBook.Page;
+            LibroDesdeDB.Datecreation = UpdateBook.Datecreation;
+            LibroDesdeDB.Cost = UpdateBook.Cost;
+
+            await _dbContext.SaveChangesAsync(); //Guarda los cambios 
+            return LibroDesdeDB;
+        }
+        public async Task<Book> crearLibro(Book CreateBook)
         {
             if(CreateBook != null)
             {
                 CreateBook.Datecreation = DateTime.Now;
-                await _dbContext.Books.AddAsync(CreateBook);
-                await _dbContext.SaveChangesAsync();
+                await _dbContext.Books.AddAsync(CreateBook); //Metodo que agrega un libro nuevo
+                await _dbContext.SaveChangesAsync(); // metodo que guarda lo anterior a la base de datos 
                 return CreateBook;
             }
             else
@@ -29,9 +45,9 @@ namespace CrudBlazorServerApp1.Repositorio
 
         public async Task DeleteBook(int BookId)
         {
-            var LibroDesdeDB = await _dbContext.Books.FindAsync(BookId);
-            _dbContext.Remove(LibroDesdeDB);
-            await _dbContext.SaveChangesAsync();
+            var LibroDesdeDB = await _dbContext.Books.FindAsync(BookId); //Metodo que busca el libro por el id
+            _dbContext.Remove(LibroDesdeDB); // metodo que elimina el libro 
+            await _dbContext.SaveChangesAsync(); // metodo que guarda lo anterior y ejecuta el borrado anterior en la base de datos
             
         }
 
@@ -51,18 +67,6 @@ namespace CrudBlazorServerApp1.Repositorio
             return _dbContext.Books.ToListAsync();
         }
 
-        public async Task<Book> UpdateBook(int BookId, Book UpdateBook)
-        {
-            var LibroDesdeDB = await _dbContext.Books.FindAsync(BookId);
-            LibroDesdeDB.Title = UpdateBook.Title;
-            LibroDesdeDB.Description = UpdateBook.Description;
-            LibroDesdeDB.Autor=UpdateBook.Autor;
-            LibroDesdeDB.Page = UpdateBook.Page;
-            LibroDesdeDB.Datecreation = UpdateBook.Datecreation;
-            LibroDesdeDB.Cost = UpdateBook.Cost;
-
-            await _dbContext.SaveChangesAsync();
-            return LibroDesdeDB;
-        }
+        
     }
 }
